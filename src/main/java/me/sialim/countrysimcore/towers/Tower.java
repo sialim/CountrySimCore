37b83cd973991864caa37ae19f01c561eb660cea
sialim <136510229+sialim@users.noreply.github.com>
2024-01-31 09:30:53 -0800

    Update Tower.java
    Improved interface

--- src/main/java/me/sialim/countrysimcore/towers/Tower.java
+++ src/main/java/me/sialim/countrysimcore/towers/Tower.java
@@ -2,6 +2,14 @@ package me.sialim.countrysimcore.towers;
 
 public interface Tower {
     String getType ();
-    double getPrice() ;
+    double getPrice () ;
     double getIncome ();
+    double getPrice (int level);
+    double getIncome (int level);
+    double getTowerIncome (Player target);
+
+    // Quantifiable Tower methods
+    int getTowers (Player target);
+    void addTower (Player target, int amount, int level);
+    void takeTower (Player target, int amount, int level);
 }
